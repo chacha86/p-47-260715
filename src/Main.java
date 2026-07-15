c00355d2f2e1d85688d5c0fa14f424a9583bd7ce
chacha86 <taejincha0619@gmail.com>
2026-07-15 12:47:05 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        }
+        // Scanner
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        String cmd = sc.nextLine();
+
+        sc.close();
     }
+}
