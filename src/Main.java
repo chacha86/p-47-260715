444363fbd2270107ac9636da874f7cfdc4d607ff
chacha86 <taejincha0619@gmail.com>
2026-07-16 10:30:35 +0900

    5단계 - 3

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,8 @@ public class Main {
         System.out.println("== 명언 앱 ==");
 
         String cmd = "";
-        int no = 1;
-        String content = "";
-        String author = "";
+        int lastNo = 0;
+        WiseSaying wiseSaying =  new WiseSaying();
 
         while(!cmd.equals("종료")) {
             System.out.print("명령) ");
@@ -22,22 +21,29 @@ public class Main {
             if(cmd.equals("등록")) {
 
                 System.out.print("명언 : ");
-                content = sc.nextLine();
+                wiseSaying.content = sc.nextLine();
                 System.out.print("작가 : ");
-                author = sc.nextLine();
+                wiseSaying.author = sc.nextLine();
                 // %d => 숫자
                 // %s => 문자열
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(no));
-                ++no;
+                ++lastNo;
+                wiseSaying.no = lastNo;
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.no));
             }
 
             if(cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                System.out.println("1 / %s / %s.".formatted(author, content));
+                System.out.println("%d / %s / %s.".formatted(wiseSaying.no, wiseSaying.author, wiseSaying.content));
             }
 
 
         }
     }
+}
+
+class WiseSaying {
+    int no;
+    String content;
+    String author;
 }
