3285c67d151553cd90c1b354732a93d014bdcf48
chacha86 <taejincha0619@gmail.com>
2026-07-16 10:07:43 +0900

    5단계 - 1

--- src/Main.java
+++ src/Main.java
@@ -28,6 +28,15 @@ public class Main {
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(no));
                 ++no;
             }
+
+            if(cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                System.out.println("2 / 작자미상 / 과거에 집착하지 마라.");
+                System.out.println("1 / 작자미상 / 현재를 사랑하라.");
+            }
+
+
         }
     }
 }
