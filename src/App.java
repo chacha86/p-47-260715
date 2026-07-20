7086d9af51ea04c32f0097425c3118ee1f60286f
chacha86 <taejincha0619@gmail.com>
2026-07-20 12:29:23 +0900

    리팩토링 - 게터/세터 도입, 생성자 도입

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
 
     Scanner sc = new Scanner(System.in);
-    int lastNo = 0; // 가장 최근 생성된 명언 번호. 번호 관리
+    int lastId = 0; // 가장 최근 생성된 명언 번호. 번호 관리
     int lastIndex = 0; // 가장 최근 생성된 명언의 배열 인덱스. 배열 관리
     WiseSaying[] wiseSayings = new WiseSaying[10];
 
@@ -42,7 +42,7 @@ public class App {
 
         for(int i = 0; i < lastIndex; i++) {
             WiseSaying w1 = wiseSayings[i];
-            if(w1.no == id) {
+            if(w1.getId() == id) {
                 targetIdx = i;
             }
         }
@@ -60,13 +60,15 @@ public class App {
 //                작가(기존) : 작자미상
 //        작가 : 홍길동
 
-        System.out.println("명언(기존)): %s".formatted(wiseSaying.content));
+        System.out.println("명언(기존)): %s".formatted(wiseSaying.getContent()));
         System.out.print("명언 : ");
-        wiseSaying.content = sc.nextLine();
+        String content = sc.nextLine();
+        wiseSaying.setContent(content);
 
-        System.out.println("작가(기존) : %s".formatted(wiseSaying.author));
+        System.out.println("작가(기존) : %s".formatted(wiseSaying.getAuthor()));
         System.out.print("작가 : ");
-        wiseSaying.author = sc.nextLine();
+        String author = sc.nextLine();
+        wiseSaying.setAuthor(author);
 
     }
 
@@ -83,7 +85,7 @@ public class App {
 
         for(int i = 0; i < lastIndex; i++) {
             WiseSaying w1 = wiseSayings[i];
-            if(w1.no == id) {
+            if(w1.getId() == id) {
                targetIdx = i;
             }
         }
@@ -104,18 +106,16 @@ public class App {
         System.out.println("----------------------");
         for (int i = lastIndex - 1; i >= 0; i--) {
             WiseSaying target = wiseSayings[i];
-            System.out.println("%d / %s / %s.".formatted(target.no, target.author, target.content));
+            System.out.println("%d / %s / %s.".formatted(target.getId(), target.getAuthor(), target.getContent()));
         }
     }
 
     private WiseSaying write(String content, String author) {
-        WiseSaying w1 = new WiseSaying();
+        ++lastId;
+        WiseSaying w1 = new WiseSaying(lastId, content, author);
         wiseSayings[lastIndex] = w1;
+
         ++lastIndex;
-        ++lastNo;
-        w1.no = lastNo;
-        w1.content = content;
-        w1.author = author;
 
         return w1;
     }
@@ -128,6 +128,6 @@ public class App {
         String author = sc.nextLine();
 
         WiseSaying wiseSaying = write(content, author);
-        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.no));
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
     }
 }
