17b5bd6b2f1a7e150fa5a9452c974ce9a3cbd917
chacha86 <taejincha0619@gmail.com>
2026-07-21 10:56:20 +0900

    paramMap으로 파라미터 갈무리

--- src/Rq.java
+++ src/Rq.java
@@ -1,9 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
 
     private String cmd;
+    Map<String, String> paramMap = new HashMap<>();
 
     public Rq(String cmd) {
         this.cmd = cmd;
+
+        // 목록?keywordType=content&keyword=과거
+        String[] cmdBits = cmd.split("\\?"); // ["목록", "keywordType=content&keyword=과거"]
+
+        String params = cmdBits[1]; //"keywordType=content&keyword=과거"
+
+        String[] paramBits = params.split("&"); //["keywordType=content", "keyword=과거"]
+
+        String param1 = paramBits[0]; //"keywordType=content"
+        String param2 = paramBits[1]; // "keyword=과거"
+
+        String[] param1Bits = param1.split("="); // ["keyword", "content"]
+        String[] param2Bits = param2.split("="); // ["keyword", "과거"]
+
+        paramMap.put(param1Bits[0], param1Bits[1]); // paramBits[0]
+        paramMap.put(param2Bits[0], param2Bits[1]);
     }
 
     public String getActionName() {
@@ -11,5 +31,18 @@ public class Rq {
 
         String[] cmdBits = cmd.split("\\?"); // ?기호를 기준으로 잘라줌. ["삭제", "id=1"]
         return cmdBits[0];
+    }
+
+    public String getParam(String key, String defaultValue) {
+
+
+
+        if(key.equals("searchKeyword")) {
+            return "영광";
+        } else if(key.equals("keywordType")) {
+            return "content";
+        }
+
+        return defaultValue;
     }
 }
--- src/RqTest.java
+++ src/RqTest.java
@@ -2,6 +2,29 @@ public class RqTest {
 
     public static void main(String[] args) {
 
+//        testActionName();
+        testGetParam();
+
+    }
+
+    public static void testGetParam() {
+        Rq rq = new Rq("목록?searchKeyword=영광"); // key=value
+        String searchKeyword = rq.getParam("searchKeyword", ""); // "영광"
+
+        System.out.println(searchKeyword); // 영광
+
+        Rq rq2 = new Rq("목록?keywordType=content"); // key=value
+        String keywordType = rq2.getParam("keywordType", "");
+
+        System.out.println(keywordType); // content
+
+        Rq rq3 = new Rq("목록?keywordType=author"); // key=value
+        String keywordType2 = rq3.getParam("keywordType", "");
+
+        System.out.println(keywordType2); // author
+    }
+
+    public static void testActionName() {
         Rq rq1 = new Rq("삭제?id=1");
         String action = rq1.getActionName();
 
